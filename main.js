a8f36fde9caf66b94df58003b166c45b5c483daf
evanmarmer <evanmarmer@gmail.com>
2023-03-27 11:46:46 -0600

    simple liker

--- main.js
+++ main.js
@@ -2,8 +2,27 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+.then(function(){
+  if ( heart.innerText === EMPTY_HEART) {
+    heart.innerText = FULL_HEART;
+    heart.style.color = "red";
+    heart.className = "activated-heart";
+  } else {
+    heart.innerText = EMPTY_HEART;
+    heart.style.color = "";
+    heart.className = "";
+  }
+})
+.catch(function(error) {
+  const modal = document.getElementById("modal");
+  modal.className = "";
+  modal.innerText = error;
+  setTimeout(() =>  modal.className = "hidden", 3000);
+});
 
+for (const glyph of articleHearts) {
+glyph.addEventListener("click", likeCallback);
+}
 
 
 
